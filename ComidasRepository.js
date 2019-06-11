aa254d5edf7e18f8b4f0a91364f6951f9ffdd5dd
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-06-10 21:14:56 -0300

    Add Schema and connect to mongodb

--- ComidasRepository.js
+++ ComidasRepository.js
@@ -1,11 +1,20 @@
-const comidas = {
-  pratosFavoritos: [
-    { 
-      id: "id12345",
-      nome: "Batata frita",
-      descricao: "Batatinha delicia"
+const mongoose = require("mongoose");
+// string de conexão:
+// mongodb://dominio:porta/nome_database
+const MONGO_URL = "mongodb://localhost:27017/reprograma";
+
+function connect () {
+  mongoose.connect(MONGO_URL,
+    { useNewUrlParser: true },
+    function (error) {
+      if(error) {
+        console.error("Deu erro: ", error)
+      } else {
+        console.log("Conectamos no mongodb!!! o/")
+      }
     }
-  ]
+  );
 }
 
-module.exports = { comidas }
+module.exports = { connect }
+
---
+++ ComidasSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+// cada schema equivale collection
+const Schema = mongoose.Schema;
+const ComidasSchema = new Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  nome: { type: String, required: true },
+  descricao: { type: String } // opcional
+})
+
+const comidasModel = mongoose.model("comidas", ComidasSchema);
+
+module.exports = comidasModel;
