c9f20c6a7dbdec16f9596a0fbbe1bffd9ff6979e
suelenquadros <suelen.quadros17@gmail.com>
2019-07-15 12:19:53 -0300

    adicionado descrição

--- OngsSchema.js
+++ OngsSchema.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const OngsSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   nome: { type: String, required: true },
-  telefone: { type: Number, required: true },
+  telefone: { type: String, required: true },
   endereco: { type: String, required: true },
   ramo: { type: String, required: true },
   email: { type: String, required: true },
